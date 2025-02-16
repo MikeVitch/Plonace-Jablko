56db0d8a9113fee7db70a1bc6d5078dee1d76589
KinuShitsuri <138403612+KinuShitsuri@users.noreply.github.com>
2025-02-16 20:38:40 +0100

    Różne rzeczy
    ++

--- Płonące-Jabłko/Assets/Scripts/Spell Scripts/Mana_Tracker.cs
+++ Płonące-Jabłko/Assets/Scripts/Spell Scripts/Mana_Tracker.cs
@@ -7,6 +7,9 @@ public class Mana_Tracker : MonoBehaviour
     public float Max_Mana = 100f;
     public float Mana_Regen = 1f;
     public float Current_Mana;
+
+    public ScreenMenu_Manager screenMenuManager;
+
     void Start()
     {
         Current_Mana = Max_Mana;
@@ -21,5 +24,6 @@ public class Mana_Tracker : MonoBehaviour
         else
             Current_Mana = Max_Mana;
         //Debug.Log(Current_Mana);
+        screenMenuManager.SetMana(Current_Mana);
     }
 }
